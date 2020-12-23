f4d85ea6a5a299858f167671e6737a0117288814
Hafizferanmi <hafizferanmi@gmail.com>
2020-12-23 08:19:31 +0100

    Graphs

--- src/Components/PageBody/VerticalGraph.js
+++ src/Components/PageBody/VerticalGraph.js
@@ -3,51 +3,35 @@ import { AreaChart, Area } from 'recharts'
 import theme from '../../theme'
 
 const data = [
-  { uv: 0 },
-  { uv: 390 },
-  { uv: 288 },
-  { uv: 270 },
-  { uv: 260 },
-  { uv: 250 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 270 },
-  { uv: 260 },
-  { uv: 250 },
-  { uv: 300 },
-  { uv: 100 },
-  { uv: 30 },
-  { uv: 200 },
-  { uv: 390 },
-  { uv: 288 },
-  { uv: 270 },
-  { uv: 260 },
-  { uv: 250 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 270 },
-  { uv: 260 },
-  { uv: 250 },
-  { uv: 250 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 300 },
-  { uv: 270 },
-  { uv: 260 },
-  { uv: 250 },
-  { uv: 300 },
-  { uv: 100 },
-  { uv: 30 },
-  { uv: 200 },
-  { uv: 390 },
-  { uv: 288 },
-  { uv: 270 },
-  { uv: 300 }
+  { data: 0 },
+  { data: 3090 },
+  { data: 288 },
+  { data: 2070 },
+  { data: 2600 },
+  { data: 250 },
+  { data: 300 },
+  { data: 2000 },
+  { data: 300 },
+  { data: 300 },
+  { data: 270 },
+  { data: 260 },
+  { data: 250 },
+  { data: 300 },
+  { data: 100 },
+  { data: 30 },
+  { data: 200 },
+  { data: 390 },
+  { data: 288 },
+  { data: 270 },
+  { data: 260 },
+  { data: 250 },
+  { data: 300 },
+
+  { data: 200 },
+  { data: 390 },
+  { data: 288 },
+  { data: 270 },
+  { data: 300 }
 ]
 
 const VerticalGraph = () => {
@@ -61,10 +45,10 @@ const VerticalGraph = () => {
       <defs>
         <linearGradient id='colorVerticalUv' x1='0' y1='0' x2='0' y2='1'>
           <stop offset='10%' stopColor={theme.colors.UIgreen} stopOpacity={0.7} />
-          <stop offset='90%' stopColor={theme.colors.Red} stopOpacity={0} />
+          <stop offset='90%' stopColor={theme.colors.UIgreen} stopOpacity={0} />
         </linearGradient>
       </defs>
-      <Area type='monotone' dataKey='uv' stroke={theme.colors.UIgreen} fillOpacity={1} fill='url(#colorVerticalUv)' />
+      <Area type='monotone' dataKey='data' stroke={theme.colors.UIgreen} fillOpacity={1} fill='url(#colorVerticalUv)' />
     </AreaChart>
   )
 }
